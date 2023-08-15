cd1175daff44040f968275c1801a6262a557bdc3
shirguin <shirguin@yandex.ru>
2023-08-15 16:03:26 +0300

    created view

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,6 @@
 <?php
 return [
     '~^hello/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayHello'],
+    '~^bye/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayBye'],
     '~^$~' => [\MyProject\Controllers\MainController::class, 'main'],
 ];
