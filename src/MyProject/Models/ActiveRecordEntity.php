259511aabda3fe8cd27593d57b30465a7f506f4f
shirguin <shirguin@yandex.ru>
2023-08-23 16:16:06 +0300

    finished lesson 21

--- src/MyProject/Models/ActiveRecordEntity.php
+++ src/MyProject/Models/ActiveRecordEntity.php
@@ -23,9 +23,63 @@ abstract class ActiveRecordEntity
         $this->$camelCaseName = $value;
     }
 
+    public function save(): void
+    {
+        $mappedProperties = $this->mapPropertiesToDbFormat();
+        if ($this->id !== null) {
+            $this->update($mappedProperties);
+        } else {
+            $this->insert($mappedProperties);
+        }
+
+    }
+
+    private function update(array $mappedProperties): void
+    {
+        $columns2params = [];
+        $params2values = [];
+        $index = 1;
+        foreach ($mappedProperties as $column => $value) {
+            $param = ':param' . $index; // :param1
+            $columns2params[] = $column . ' = ' . $param; // column1 = :param1
+            $params2values[$param] = $value; // [:param1 => value1]
+            $index++;
+        }
+        $sql = 'UPDATE ' . static::getTableName() . ' SET ' . implode(', ', $columns2params) . ' WHERE id = ' . $this->id;
+        $db = Db::getInstance();
+        $db->query($sql, $params2values, static::class);
+        
+    }
+
+
+    private function insert(array $mappedProperties): void
+    {
+        //здесь мы создаём новую запись в базе
+    }
+
+    private function mapPropertiesToDbFormat(): array
+    {
+        $reflector = new \ReflectionObject($this);
+        $properties = $reflector->getProperties();
+
+        $mappedPropperties = [];
+        foreach ($properties as $property) {
+            $proppertyName = $property->getName();
+            $propertyNameAsUnderscope = $this->camelCaseToUnderscore($proppertyName);
+            $mappedPropperties[$propertyNameAsUnderscope] = $this->$proppertyName;
+        }
+
+        return $mappedPropperties;
+    }
+
     private function underscoreToCamelCase(string $source): string
     {
         return lcfirst(str_replace('_', '', ucwords($source, '_')));
+    }
+
+    private function camelCaseToUnderscore(string $source): string
+    {
+        return strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $source));
     }
 
     /**
@@ -33,7 +87,7 @@ abstract class ActiveRecordEntity
      */
     public static function findAll(): array
     {
-        $db = new Db();
+        $db = Db::getInstance();
         return $db->query('SELECT * FROM `' . static::getTableName() . '`;', [], static::class);
     }
 
@@ -45,7 +99,7 @@ abstract class ActiveRecordEntity
      */
     public static function getById(int $id): ?self
     {
-        $db = new Db();
+        $db = Db::getInstance();
         $entities = $db->query(
             'SELECT * FROM `' . static::getTableName() . '` WHERE id=:id;',
             [':id' => $id],
